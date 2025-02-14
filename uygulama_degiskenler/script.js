eb84f59de39bca3697651628e9a6c395a6532693
HarunnMetin <mtnmusa07@gmail.com>
2025-02-14 23:58:42 +0300

    degiskenler-uygulama

---
+++ uygulama_degiskenler/script.js
@@ -0,0 +1,31 @@
+console.log("-----Ogrenci 1-----");
+
+console.log("");
+
+var ogr1_ad = "Selcuk";
+var ogr1_soyad = "Lale";
+var ogr1_dogumTarihi = 2008;
+var ogr1_math1 = 70;
+var ogr1_math2 = 100;
+var ogr1_math3 = 90;
+var ogr1_yas = 2025 - ogr1_dogumTarihi;
+var ogr1_ortalama = (ogr1_math1 + ogr1_math2 + ogr1_math3) / 3;
+console.log("Ortalama: " + parseInt(ogr1_ortalama));
+console.log("Ogrenci 1 yas: " + ogr1_yas);
+console.log("Ogrenci gecti mi: " + (ogr1_ortalama >= 50));
+
+console.log("-----Ogrenci 2------");
+
+console.log("");
+
+var ogr2_ad = "Demir";
+var ogr2_soyad = "Acar";
+var ogr2_dogumTarihi = 2010;
+var ogr2_math1 = 60;
+var ogr2_math2 = 10;
+var ogr2_math3 = 15;
+var ogr2_yas = 2025 - ogr2_dogumTarihi;
+var ogr2_ortalama = (ogr2_math1 + ogr2_math2 + ogr2_math3) / 3;
+console.log("Ortalama: " + parseInt(ogr2_ortalama));
+console.log("Ogrenci 2 yas: " + ogr2_yas);
+console.log("Ogrenci gecti mi: " + (ogr2_ortalama >= 50));
