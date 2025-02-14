e5181d6bd45c98b93cfef2adf7103577afb1a1ea
HarunnMetin <mtnmusa07@gmail.com>
2025-02-14 22:41:31 +0300

    string-number-boolian

---
+++ degisken_turleri/script.js
@@ -0,0 +1,26 @@
+var urunAdi = "kalem";
+var urunFiyati = 100;
+console.log(typeof urunAdi);
+console.log(typeof urunFiyati);
+
+var sayi1 = "30";
+var sayi2 = "50";
+
+console.log(sayi1 + sayi2);
+
+var ad = "Harun";
+var soyad = "metin";
+
+ad = "Universe";
+soyad = "space";
+
+console.log(ad + " " + soyad);
+
+var sinavNotu = 40;
+var gectiMi = sinavNotu >= 50;
+console.log(gectiMi);
+
+var age;
+
+console.log(age);
+console.log(typeof age);
