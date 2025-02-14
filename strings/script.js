a8f882629d3d7f471751f971e39b8ec2bd5c790a
HarunnMetin <mtnmusa07@gmail.com>
2025-02-15 00:16:05 +0300

    strings

---
+++ strings/script.js
@@ -0,0 +1,13 @@
+var ad = "Harun";
+
+console.log(ad[0]);
+console.log(ad[1]);
+
+var soyad = "Metin";
+var yas = 40;
+var sehir = "Antalya";
+
+var mesaj =
+  "Benim Adim: " + ad + " ve soyadim " + soyad + ". " + sehir + "'de yasiyorum";
+
+console.log(mesaj);
