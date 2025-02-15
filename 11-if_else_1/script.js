0a44cbe916d7599b80378ba8316af88502f1991f
HarunnMetin <mtnmusa07@gmail.com>
2025-02-15 23:10:26 +0300

    if-else

---
+++ 11-if_else_1/script.js
@@ -0,0 +1,25 @@
+let a = 10;
+let b = 3;
+
+if (a < b) {
+  console.log("Buyuktur");
+}
+let username = "darkside";
+let parola = "123457";
+
+let kosul = (username == "darkside");
+
+
+if (kosul) {
+  if(parola=="12345"){
+    console.log("Uygulamaya Giris Yapildi");
+  }
+  else{
+    console.log("parola hatali")
+  }
+  
+} else {
+  console.log("kullanici adi hatali");
+}
+
+
