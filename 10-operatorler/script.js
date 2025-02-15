5c2fd905417ff82e965761870233fce869bf7fc6
HarunnMetin <mtnmusa07@gmail.com>
2025-02-15 21:51:19 +0300

    operator

---
+++ 10-operatorler/script.js
@@ -0,0 +1,22 @@
+let sonuc;
+let a = 20,
+  b = 40,
+  c = 5;
+
+sonuc = a + b;
+sonuc = a - b;
+sonuc = a * b;
+sonuc = a / b;
+sonuc = a % b;
+sonuc = b--;
+// sonuc = c++;
+sonuc = ++c;
+sonuc = c;
+
+sonuc = sonuc + a;
+sonuc = a == b;
+sonuc = a != b;
+
+console.log(sonuc);
+// console.log(b);
+// console.log(c);
