698245dd1aaf13735cddc908a33bf6775fd8863a
HarunnMetin <mtnmusa07@gmail.com>
2025-02-15 19:41:29 +0300

    array

---
+++ 7-diziler/script.js
@@ -0,0 +1,15 @@
+let urun1 = "Kalem";
+let urun2 = "Silgi";
+let urun3 = "Cetvel";
+
+let urunler = ["Kalem", "Silgi", "Cetvel"];
+let fiyatlar = [20, 15, 80];
+let renkler = ["Sari", "Siyah", "Pembe"];
+
+let sonuc = `${urunler[0]}`;
+sonuc = `${renkler[2]}`;
+sonuc = `${urunler[0]} - ${fiyatlar[0]} - ${renkler[0]}`;
+sonuc = `${urunler[1]} - ${fiyatlar[1]} - ${renkler[1]}`;
+sonuc = `${urunler[2]} - ${fiyatlar[2]} - ${renkler[2]}`;
+
+console.log(sonuc);
