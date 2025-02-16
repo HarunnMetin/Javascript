7910c0dba5f6e58865bd00c89a4acd0a8a79582a
HarunnMetin <mtnmusa07@gmail.com>
2025-02-17 01:19:13 +0300

    scopes

---
+++ 17-scopes/script.js
@@ -0,0 +1,13 @@
+var isim = "Derin";
+var yas = 25;
+
+function yazdir(){
+    var isim = "Fatma"
+    var yas = 20;
+    console.log(isim);
+    console.log(yas);
+}
+
+yazdir();
+console.log(isim);
+console.log(yas);
