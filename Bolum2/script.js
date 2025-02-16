e9afe0858caad053a003327b523fac5edce444ed
HarunnMetin <mtnmusa07@gmail.com>
2025-02-17 01:58:58 +0300

    document_object_model

---
+++ Bolum2/script.js
@@ -0,0 +1,28 @@
+let sonuc;
+
+sonuc = document.all;
+sonuc = document.all[6];
+sonuc = document.all.length;
+sonuc = document.documentElement;
+
+sonuc = document.head;
+sonuc = document.body;
+sonuc = document.body.children;
+
+sonuc = document.doctype;
+sonuc = document.URL;
+sonuc = document.characterSet;
+sonuc = document.contentType;
+
+sonuc = document.forms[0];
+sonuc = document.forms[0].method;
+sonuc = document.forms[0].action;
+
+sonuc = document.links;
+sonuc = document.links[0].id="Hepsi";
+sonuc = document.links[0].innerHTML="Tumu";
+sonuc = document.links[0].className;
+sonuc = document.images;
+sonuc = document.images[0];
+
+console.log(sonuc);
