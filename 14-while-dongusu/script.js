e214316713540ec6a0b9b45af5c2b3de7136b66a
HarunnMetin <mtnmusa07@gmail.com>
2025-02-16 17:31:21 +0300

    while-do_while

---
+++ 14-while-dongusu/script.js
@@ -0,0 +1,27 @@
+for(let i = 0; i <= 25; i++){
+  console.log(i);
+}
+
+
+console.log("*****************************************")
+let sayi = 10;
+
+while(sayi <= 17){
+
+  if(sayi % 2 == 0){
+
+    console.log(sayi);
+  } 
+
+  sayi++;
+}
+sayi = 30;
+do{
+
+  console.log(sayi);
+  sayi++;
+
+}while(sayi <= 90){
+
+  
+}
