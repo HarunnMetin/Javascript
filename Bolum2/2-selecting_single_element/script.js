41ec88c2bb8acdc248b628a99b3a2405c7371e08
HarunnMetin <mtnmusa07@gmail.com>
2025-02-17 16:48:45 +0300

    single-element

---
+++ Bolum2/2-selecting_single_element/script.js
@@ -0,0 +1,43 @@
+// let sonuc;
+
+// sonuc = document.getElementById("title");
+// // sonuc = document.getElementById("title").id;
+// // sonuc = document.getElementById("title").classList;
+// // sonuc = document.getElementById("title").className;
+
+// document.getElementById("title").style.fontSize = "50px";
+// document.getElementById("title").style.color = "#FFDDAE";
+// // document.getElementById("title").style.display = "none";
+
+// document.getElementById("title").innerText = "Technology";
+// document.getElementById("title").innerHTML = "<p>Technology</p>";
+
+// console.log(sonuc);
+
+// console.log("ææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææ")
+
+// let sonuc2;
+
+// sonuc2 = document.querySelector(".text-center");
+// sonuc2 = document.querySelector("#title");
+// document.querySelector("h1");
+// sonuc2 = document.querySelector("input[type='submit']");
+// sonuc2 = document.querySelector("input");
+// sonuc2 = document.querySelector("input[type='submit']");
+
+// document.querySelector("li");
+
+const parentElement = document.querySelector("ul");
+parentElement.style.listStyleType ="none";
+
+console.log(parentElement)
+
+const firsElement = parentElement.querySelector("li");
+firsElement.style.color = "green";
+
+
+console.log(parentElement)
+
+
+
+
